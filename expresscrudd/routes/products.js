dab07b875e14135d421e0986921f793ace3ab7d8
SyedKomail <SyedKomail@users.noreply.github.com>
2021-04-16 23:53:19 -0700

    express product.js

---
+++ expresscrudd/routes/products.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var Product = require("../models/product");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ let products = Product.find();
+ console.log(products);
+  res.render("products/list" );
+  
+});
+
+module.exports = router;
